58156a5c41f0ae2d86d48fa1e0ad90021df343ed
Mo Weiss <mshwss@gmail.com>
2019-01-23 15:53:51 -0500

    initial commit

---
+++ ArrraysListsDictionaries/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArrraysListsDictionaries
+{
+    public class Car
+    {
+
+        public Car(string make, string model, int year)
+    {
+            Make = make;
+            Model = model;
+            Year = year;
+    }
+    
+        
+        public string Make { get; set;}
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int MilesPerGallon { get; set; }
+        
+    }
+}
---
+++ ArrraysListsDictionaries/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrraysListsDictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fordFiesta = new Car("Ford", "Fiesta", 2013);
+            var subaruLegacy = new Car("Subaru", "Legacy", 2017);
+            var hondaCrv = new Car("Honda", "CRV", 2014);
+
+            var parkedCars = new Car[5];
+            Console.WriteLine(parkedCars.Length);
+
+            parkedCars[0] = hondaCrv;
+
+            var firstCar = parkedCars[0];
+            Console.WriteLine(firstCar.Make);
+
+            var luxuryCars = new List<Car>();
+
+            luxuryCars[0] = hondaCrv;
+            luxuryCars.Add(firstCar);
+
+            var firstLuxuryCar = luxuryCars[0];
+        
+                
+            Console.ReadLine();
+        }
+    }
+}
